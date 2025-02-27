03710777038ea803a8b10f605ddb41a69b7a023f
Frank Martin Moreno Alburqueque <admin@fmorenoadmin.com.pe>
2025-02-27 13:23:42 -0500

    cambios

--- israel_cc_1/constant.php
+++ israel_cc_1/constant.php
@@ -6,4 +6,4 @@
 	//define('DB_TYPE', 'pg_');
 	//define('DB_TYPE', 'sqlsrv_');
 	//---------------------------------
-	define('URL', 'https://localhost/pruebas/israel_cc_1/');
+	define('URL', 'https://localhost/php_latam/israel_cc_1/');
