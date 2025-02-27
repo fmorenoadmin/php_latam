4599da75d05ff95da487a9a3f1139158cbbeb0d2
Frank Martin Moreno Alburqueque <admin@fmorenoadmin.com.pe>
2025-02-27 13:22:36 -0500

    Initial commit

---
+++ israel_cc_1/constant.php
@@ -0,0 +1,9 @@
+<?php
+	define('SCHU', '_prd');
+	//define('SCHU', '_qas');
+	//---------------------------------
+	define('DB_TYPE', 'mysqli_');
+	//define('DB_TYPE', 'pg_');
+	//define('DB_TYPE', 'sqlsrv_');
+	//---------------------------------
+	define('URL', 'https://localhost/pruebas/israel_cc_1/');
